4601618a128acfbd410020f94be157f80c6e92a2
Isaac Joseph Enriquez <isaacjosephriveraenriquez.1@gmail.com>
2024-04-23 07:41:13 +0800

    04/23/202432

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ const PORT = Number(process.env.POSTGRES_PORT);
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.POSTGRES_HOST,
-      port: PORT,
+      port: 5432,
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DATABASE,
